1a729765b4c139186871a5214c4fc4acb0345ab8
yuhsuanyang <sb0953330882@gmail.com>
2021-06-28 11:24:49 +0800

    feat: add candlestick plot

--- web_django/price/util.py
+++ web_django/price/util.py
@@ -19,8 +19,9 @@ def query_historical_price(stock_code, end_date):
     url = f"https://query1.finance.yahoo.com/v7/finance/download/{stock_code}.TW?period1={start}&period2={end}&interval=1d&events=history&crumb=DCkS0u002FOZyL"
     # print(url)
     res = requests.get(url)
-    data = pd.read_csv(StringIO(res.text))[['Date', 'Open', 'Close', 'Volume']]
-    data.columns = ['date', 'open', 'daily', 'volume']
+    data = pd.read_csv(StringIO(
+        res.text))[['Date', 'Open', 'High', 'Low', 'Close', 'Volume']]
+    data.columns = ['date', 'open', 'high', 'low', 'daily', 'volume']
     data['5MA'] = data.daily.rolling(5).mean()
     data['20MA'] = data.daily.rolling(20).mean()
     data['60MA'] = data.daily.rolling(60).mean()
@@ -28,24 +29,26 @@ def query_historical_price(stock_code, end_date):
 
 
 def create_dash(stock_code, company_name, df):
-    features = ['daily', '5MA', '20MA', '60MA']
+    features = ['daily', '5MA', '20MA', '60MA', 'k線']
+    slider_style = {'margin-right': '-100px'}
     app = DjangoDash('Price_Dashboard')
     app.layout = html.Div(
         [
             html.H3(id='title',
                     children="近90天股價走勢",
                     style={'text-align': 'center'}),
-            dcc.Checklist(id='checkbox',
-                          options=[{
-                              'label': features[i],
-                              'value': i
-                          } for i in range(len(features))],
-                          value=[i for i in range(len(features))],
-                          style={
-                              'width': '100%',
-                              'height': '20px',
-                              'text-align': 'center'
-                          }),
+            dcc.Checklist(
+                id='checkbox',
+                options=[{
+                    'label': features[i],
+                    'value': i
+                } for i in range(len(features))],
+                value=[i for i in range(len(features) - 1)],  # 預設沒有k線
+                style={
+                    'width': '100%',
+                    'height': '20px',
+                    'text-align': 'center'
+                }),
             dcc.Graph(id='line_plot',
                       style={
                           'width': '100%',
@@ -65,9 +68,21 @@ def create_dash(stock_code, company_name, df):
                             value=[len(df) - 90, len(df) - 1],
                             step=1,
                             marks={
-                                0: df.date[0],
-                                len(df) - 90: df.date.iloc[-90],
-                                len(df) - 1: df.date.iloc[-1]
+                                0: {
+                                    'label': df.date[0],
+                                    'style': slider_style
+                                },
+                                len(df) - 90: {
+                                    'label': df.date.iloc[-90],
+                                    'style': {
+                                        'margin-top': '-40px',
+                                        'margin-right': '-100px'
+                                    }
+                                },
+                                len(df) - 1: {
+                                    'label': df.date.iloc[-1],
+                                    'style': slider_style
+                                }
                             })
         ],
         style={
@@ -86,31 +101,61 @@ def create_dash(stock_code, company_name, df):
                   [Input('checkbox', 'value'),
                    Input('slider', 'value')])
     def update_line_chart(contents, date_range):
-        #        features = ['daily'] + [self.features[i] for i in contents]
         selected_features = [features[i] for i in contents]
+        line_colors = ['dimgray', 'dodgerblue', 'violet', 'orange']
         fig = go.Figure()
-        x = df.date.iloc[date_range[0]:date_range[1] + 1]
-        for col in selected_features:
-            fig.add_trace(
-                go.Scatter(x=x,
-                           y=df[col].iloc[date_range[0]:date_range[1] +
-                                          1].values.reshape(-1),
-                           mode='lines',
-                           name=col))
+        selected_data = df.iloc[date_range[0]:date_range[1] + 1]
+        x = selected_data.date
+        #for col in selected_features:
+        for i in contents:
+            col = features[i]
+            if col == "k線":
+                fig.add_trace(
+                    go.Candlestick(x=x,
+                                   open=selected_data['open'],
+                                   high=selected_data['high'],
+                                   low=selected_data['low'],
+                                   close=selected_data['daily'],
+                                   name="k線"))
+
+            else:
+                fig.add_trace(
+                    go.Scatter(x=x,
+                               y=selected_data[col],
+                               mode='lines',
+                               name=col,
+                               marker_color=line_colors[i]))
         fig.update_layout(title={
             'y': 0.9,
             'x': 0.5,
             'xanchor': 'center',
             'yanchor': 'top',
         },
-                          yaxis_title='$NTD')
+                          yaxis_title='$NTD',
+                          xaxis_rangeslider_visible=False)
         return fig
 
     @app.callback(Output('slider', 'marks'), [Input('slider', 'value')])
     def update_slider_mark(date_range):
         start = df.date.iloc[date_range[0]]
         end = df.date.iloc[date_range[1]]
-        return {0: df.date[0], date_range[0]: start, date_range[1]: end}
+        return {
+            0: {
+                'label': df.date[0],
+                'style': slider_style
+            },
+            date_range[0]: {
+                'label': start,
+                'style': {
+                    'margin-top': '-40px',
+                    'margin-right': '-100px'
+                }
+            },
+            date_range[1]: {
+                'label': end,
+                'style': slider_style
+            }
+        }
 
     @app.callback(Output('bar_chart', 'figure'), [Input('slider', 'value')])
     def update_bar_chart(date_range):
