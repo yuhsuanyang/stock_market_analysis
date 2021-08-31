95ab984125439193fd7a37da883e6cafcebb6ab4
yuhsuanyang <sb0953330882@gmail.com>
2021-08-31 17:52:39 +0800

    refactor: modify code

--- web_django/dividend/update_db.py
+++ web_django/dividend/update_db.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from .models import DividendData
-from meta_data.model import StockMetaData
+from meta_data.models import StockMetaData
 
 meta_data = StockMetaData.objects.all()
 stocks = [stock.code for stock in meta_data]
@@ -8,16 +8,23 @@ stocks = [stock.code for stock in meta_data]
 
 def query_dividend(stock_code):
     url = f"https://tw.stock.yahoo.com/d/s/dividend_{stock_code}.html"
-    season_dict = {'第一季': 1, '第二季': 2, '第三季': 3, '第四季': 4,
-                  '上半': 1.5, '下半': 2.5}
+    season_dict = {
+        '第一季': 1,
+        '第二季': 2,
+        '第三季': 3,
+        '第四季': 4,
+        '上半': 1.5,
+        '下半': 2.5
+    }
     df = pd.read_html(url, encoding="cp950")[3][[0, 1, 2, 5, 6]]
-    df.columns = ['year', 'distribute_date',
-                  'cash_dividend', 'stock_dividend', 'total']
+    df.columns = [
+        'year', 'distribute_date', 'cash_dividend', 'stock_dividend', 'total'
+    ]
     df = df.drop(0)
     df = df[df.distribute_date != '-'].reset_index(drop=True)
-    
+
     processed_data = []
-    
+
     for i in range(len(df)):
         year = df.iloc[i].year.split('年')
         #print(year)
@@ -25,23 +32,26 @@ def query_dividend(stock_code):
             season = season_dict[year[1]]
         else:
             season = 0
-            
-        processed_data.append([int(year[0]),
-                               season,
-                               df.iloc[i].distribute_date,
-                               df.iloc[i].cash_dividend,
-                               df.iloc[i].stock_dividend,
-                               df.iloc[i].total,
-                               ])
 
-   
+        processed_data.append([
+            int(year[0]),
+            season,
+            df.iloc[i].distribute_date,
+            df.iloc[i].cash_dividend,
+            df.iloc[i].stock_dividend,
+            df.iloc[i].total,
+        ])
 
-    df = pd.DataFrame(processed_data, columns=['year', 'season', 'distribute_date',
-                  'cash_dividend', 'stock_dividend', 'total'])
+    df = pd.DataFrame(processed_data,
+                      columns=[
+                          'year', 'season', 'distribute_date', 'cash_dividend',
+                          'stock_dividend', 'total'
+                      ])
     return df
 
+
 def main():
-    DividendData.objects.delete()
+    DividendData.objects.all().delete()
     for stock_code in stocks:
         df = query_dividend(stock_code)
         for i in range(len(df)):
@@ -53,5 +63,3 @@ def main():
                                stock=df.iloc[i].stock_dividend)
             row.save()
         print(stock_code)
-        
-        
