554ac1bea767f812e5a7ff3021e5809346699ea1
yuhsuanyang <sb0953330882@gmail.com>
2021-10-10 23:36:51 +0800

    refactor: move style of base_dashboard to styles.css

--- web_django/web_django/settings.py
+++ web_django/web_django/settings.py
@@ -117,7 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, '/static/')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'meta_data/static/'),
+    #    os.path.join(BASE_DIR, '/templates/')
+]
+print(STATICFILES_DIRS)
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
