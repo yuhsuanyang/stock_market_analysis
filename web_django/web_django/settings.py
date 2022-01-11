23c2b04bdc8a2104d7de987b2e7e4deaf5ce9eb5
yuhsuanyang <sb0953330882@gmail.com>
2022-01-11 17:17:28 +0800

    add allow host in settings.py

--- web_django/web_django/settings.py
+++ web_django/web_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#ak#+#$pw8k=s6aox_g%7tg098#kx05wch3y9&1uuovlz0zy##
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['urexpenses.herokuapp.com/']
 
 # Application definition
 
